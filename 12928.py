0f5e6e006421ebeb3e8e135c3598d5d70beffeb4
안상준 <justin_a@yonsei.ac.kr>
2020-09-13 21:11:51 +0900

    Update 12928.py

--- 12928.py
+++ 12928.py
@@ -2,5 +2,5 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12928
 
 def solution(n):
-    answer = sum([x for x in range(1,n+1) if n%x ==0])
+    answer = sum([x for x in range(1, n + 1) if n % x == 0])
     return answer
