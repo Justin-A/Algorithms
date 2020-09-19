363d4996bad403e24cdd86b3f9addce1b7c6c1bb
안상준 <justin_a@yonsei.ac.kr>
2020-09-19 23:58:39 +0900

    Update 68644.py

--- 68644.py
+++ 68644.py
@@ -2,7 +2,7 @@ def solution(numbers):
     sample_lst = []
     for i in range(len(numbers)):
         for j in range(len(numbers)):
-            if i != j:
+            if i < j:
                 sample_lst.append(numbers[i] + numbers[j])
                 
     answer = sorted(list(set(sample_lst)))
